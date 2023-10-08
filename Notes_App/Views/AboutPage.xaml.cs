fd2c5bec26e363566c3c3c0d03bca0d9d45a1b61
Giacomo <giacomo.corso@gmail.com>
2023-10-08 13:57:54 +0200

    data binding in AboutPage using model About

--- Notes_App/Views/AboutPage.xaml.cs
+++ Notes_App/Views/AboutPage.xaml.cs
@@ -2,13 +2,16 @@ namespace Notes_App.Views;
 
 public partial class AboutPage : ContentPage
 {
-	public AboutPage()
-	{
-		InitializeComponent();
-	}
+    public AboutPage()
+    {
+        InitializeComponent();
+    }
 
-	private async void LearnMore_Clicked(object sender, EventArgs e)
-	{
-		await Launcher.Default.OpenAsync("https://aka.ms/maui");
-	}
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is Models.About about)
+        {
+            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
+        }
+    }
 }
