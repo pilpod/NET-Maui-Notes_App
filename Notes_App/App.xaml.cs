11005daa4793eae55b640fe6e6bfeba500e3468f
Giacomo <giacomo.corso@gmail.com>
2023-10-08 10:47:25 +0200

    Agregar archivos de proyecto.

---
+++ Notes_App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Notes_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
