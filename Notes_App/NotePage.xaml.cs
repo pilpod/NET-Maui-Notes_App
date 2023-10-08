195dae6d738a975104cd342ca833c562d524fa8c
Giacomo <giacomo.corso@gmail.com>
2023-10-08 12:49:48 +0200

    button in NotePage setted

--- Notes_App/NotePage.xaml.cs
+++ Notes_App/NotePage.xaml.cs
@@ -2,8 +2,30 @@ namespace Notes_App;
 
 public partial class NotePage : ContentPage
 {
+	string _filename = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
 	public NotePage()
 	{
 		InitializeComponent();
-	}
+
+        if (File.Exists(_filename))
+        {
+            TextEditor.Text = File.ReadAllText(_filename);
+        }
+    }
+
+    private void SaveButton_Clicked(object sender, EventArgs e)
+    {
+        File.WriteAllText(_filename, TextEditor.Text);
+    }
+
+    private void DeleteButton_Clicked(Object sender, EventArgs e)
+    {
+        if (File.Exists(_filename))
+        {
+            File.Delete(_filename);
+            TextEditor.Text = string.Empty;
+        }
+    }
+
+
 }
