f691d048155fa6357bb292de486c702214bd44fe
Giacomo <giacomo.corso@gmail.com>
2023-10-11 12:13:32 +0200

    AllNotesViewModel,AllNotesPage done. refactored with mvvm

--- Notes_App/Views/NotePage.xaml.cs
+++ Notes_App/Views/NotePage.xaml.cs
@@ -1,9 +1,7 @@
 namespace Notes_App.Views;
-[QueryProperty(nameof(ItemId), nameof(ItemId))]
+
 public partial class NotePage : ContentPage
 {
-    public string ItemId { set { LoadNote(value); } }
-
     public NotePage()
     {
         InitializeComponent();
