95dc8601977c917ea73c96ef82d6020d23dca388
Giacomo <giacomo.corso@gmail.com>
2023-10-11 11:42:30 +0200

    NoteViewModel,NotePage refactored

--- Notes_App/Views/NotePage.xaml.cs
+++ Notes_App/Views/NotePage.xaml.cs
@@ -4,49 +4,8 @@ public partial class NotePage : ContentPage
 {
     public string ItemId { set { LoadNote(value); } }
 
-	public NotePage()
-	{
-		InitializeComponent();
-
-        string appDataPath = FileSystem.AppDataDirectory;
-        string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
-
-        LoadNote(Path.Combine(appDataPath, randomFileName));
-    }
-
-    private void LoadNote(string filename)
+    public NotePage()
     {
-        Models.Note noteModel = new Models.Note();
-        noteModel.Filename = filename;
-
-        if (File.Exists(filename))
-        {
-            noteModel.Date = File.GetCreationTime(filename);
-            noteModel.Text = File.ReadAllText(filename);
-        }
-
-        BindingContext = noteModel;
+        InitializeComponent();
     }
-
-    private async void SaveButton_Clicked(object sender, EventArgs e)
-    {
-        if (BindingContext is Models.Note note)
-            File.WriteAllText(note.Filename, TextEditor.Text);
-
-        await Shell.Current.GoToAsync("..");
-    }
-
-    private async void DeleteButton_Clicked(object sender, EventArgs e)
-    {
-        if (BindingContext is Models.Note note)
-        {
-            // Delete the file.
-            if (File.Exists(note.Filename))
-                File.Delete(note.Filename);
-        }
-
-        await Shell.Current.GoToAsync("..");
-    }
-
-
 }
